cd5ee5a767e61554507674c9f312ceae5a9a52e5
yixvan6 <yixvan6@163.com>
2020-05-29 03:54:28 +0000

    新建话题

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,3 +1,7 @@
 <div class="card">
-  <div class="card-body">导航栏</div>
+  <div class="card-body">
+    <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-label="Left Aligin">
+      <i class="fas fa-pencil-alt mr-2"></i>  新建话题
+    </a>
+  </div>
 </div>
