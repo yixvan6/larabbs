c09ae56f9505f8e5302d9686de50463d0cbe19e6
yixvan6 <yixvan6@163.com>
2020-05-29 09:50:29 +0000

    删除话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -67,7 +67,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '话题删除成功');
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
