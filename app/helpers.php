c8dcde565cdfdecb7a656ad37bbbf4efa4e40b46
yixvan6 <yixvan6@163.com>
2020-06-04 08:06:06 +0000

    后台 - 内容管理

--- app/helpers.php
+++ app/helpers.php
@@ -15,3 +15,29 @@ function make_excerpt($topic_body, $length = 160)
     $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($topic_body)));
     return Str::limit($excerpt, $length);
 }
+
+function model_admin_link($title, $model)
+{
+    return model_link($title, $model, 'admin');
+}
+
+function model_link($title, $model, $prefix = '')
+{
+    $model_name = model_plural_name($model);
+
+    $prefix = $prefix ? "/$prefix/" : '/';
+
+    $url = config('app.url') . $prefix . $model_name . '/' . $model->id;
+
+    return '<a href="' . $url .'" target="_blank">' . $title . '</a>';
+}
+
+// 将模型的类名转为复数蛇形命名
+function model_plural_name($model)
+{
+    $class_name = class_basename($model);
+
+    $snake_name = Str::snake($class_name);
+
+    return Str::plural($snake_name);
+}
